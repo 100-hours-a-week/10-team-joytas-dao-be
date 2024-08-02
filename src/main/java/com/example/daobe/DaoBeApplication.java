129c4c64ae1b2dbbbffad7b48008104290b25efb
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-02 17:04:45 +0900

    chore: 초기 프로젝트 설정

---
+++ src/main/java/com/example/daobe/DaoBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.daobe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaoBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/daobe/DaoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.daobe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

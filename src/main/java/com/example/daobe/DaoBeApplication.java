61976a3bf1ef4b8a1b3e767868ae5f7105f3ed13
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-02 17:13:39 +0900

    chore: 프로퍼티 설정 스캐너 추가

--- src/main/java/com/example/daobe/DaoBeApplication.java
+++ src/main/java/com/example/daobe/DaoBeApplication.java
@@ -2,12 +2,14 @@ package com.example.daobe;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class DaoBeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DaoBeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DaoBeApplication.class, args);
+    }
 
 }

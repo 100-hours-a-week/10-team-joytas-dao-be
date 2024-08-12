48d0df41469c2c9131183c113416ce7eb386718f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-12 21:50:28 +0900

    feat: 라운지 서비스 구성

--- src/main/java/com/example/daobe/lounge/service/LoungeService.java
+++ src/main/java/com/example/daobe/lounge/service/LoungeService.java
@@ -1,2 +1,14 @@
-package com.example.daobe.lounge.service;public class LoungeService {
+package com.example.daobe.lounge.service;
+
+import com.example.daobe.lounge.repository.LoungeRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class LoungeService {
+
+    private final LoungeRepository loungeRepository;
 }

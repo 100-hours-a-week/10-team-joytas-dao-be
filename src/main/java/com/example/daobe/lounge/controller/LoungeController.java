c95629ea4dc72b93be30638c0ea83830f9e82a2e
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-13 13:13:34 +0900

    feat: 라운지 컨트롤러 및 서비스 구현

--- src/main/java/com/example/daobe/lounge/controller/LoungeController.java
+++ src/main/java/com/example/daobe/lounge/controller/LoungeController.java
@@ -1,8 +1,16 @@
 package com.example.daobe.lounge.controller;
 
+import com.example.daobe.common.response.ApiResponse;
+import com.example.daobe.lounge.dto.LoungeCreateRequestDto;
+import com.example.daobe.lounge.dto.LoungeCreateResponseDto;
 import com.example.daobe.lounge.service.LoungeService;
+import com.example.daobe.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +21,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class LoungeController {
 
     private final LoungeService loungeService;
+    private final UserService userService;
 
+    // TODO: ApiResponse 응답 메시지 Enum 으로 관리 하도록 구현
+    @PostMapping
+    public ResponseEntity<ApiResponse<LoungeCreateResponseDto>> generateLounge(
+            @RequestBody LoungeCreateRequestDto request) {
+        User findUser = userService.findById(request.userId());
+        LoungeCreateResponseDto loungeCreateResponse = loungeService.create(request, findUser);
+        ApiResponse<LoungeCreateResponseDto> response = new ApiResponse<>("LOUNGE_CREATED_SUCCESS",
+                loungeCreateResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
 }
--- src/main/java/com/example/daobe/lounge/service/LoungeService.java
+++ src/main/java/com/example/daobe/lounge/service/LoungeService.java
@@ -1,6 +1,11 @@
 package com.example.daobe.lounge.service;
 
+import com.example.daobe.lounge.dto.LoungeCreateRequestDto;
+import com.example.daobe.lounge.dto.LoungeCreateResponseDto;
+import com.example.daobe.lounge.entity.Lounge;
+import com.example.daobe.lounge.entity.LoungeStatus;
 import com.example.daobe.lounge.repository.LoungeRepository;
+import com.example.daobe.user.entity.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,4 +16,18 @@ import org.springframework.stereotype.Service;
 public class LoungeService {
 
     private final LoungeRepository loungeRepository;
+
+    public LoungeCreateResponseDto create(LoungeCreateRequestDto request, User user) {
+        Lounge lounge = Lounge.builder()
+                .user(user)
+                .name(request.name())
+                .type(request.type())
+                .status(LoungeStatus.ACTIVE)    // 라운지 활성화
+                .reason(null)
+                .reasonDetail(null)
+                .build();
+
+        Lounge savedLounge = loungeRepository.save(lounge);
+        return savedLounge.toLoungeCreateResponseDto();
+    }
 }

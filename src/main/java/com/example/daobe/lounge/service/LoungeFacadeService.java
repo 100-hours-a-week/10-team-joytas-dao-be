d97c134aef982305a017a794d4b42c0aabd9026a
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-13 14:39:19 +0900

    fix: UserRepository import 되지 않은 오류 수정

--- src/main/java/com/example/daobe/lounge/service/LoungeFacadeService.java
+++ src/main/java/com/example/daobe/lounge/service/LoungeFacadeService.java
@@ -3,6 +3,7 @@ package com.example.daobe.lounge.service;
 import com.example.daobe.lounge.dto.LoungeCreateRequestDto;
 import com.example.daobe.lounge.dto.LoungeCreateResponseDto;
 import com.example.daobe.user.entity.User;
+import com.example.daobe.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;

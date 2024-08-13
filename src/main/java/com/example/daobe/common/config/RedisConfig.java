3a2a40ccb49038372abb9e101e68f971f2df04b7
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-13 22:12:36 +0900

    feat: redis 연결 설청 추가

---
+++ src/main/java/com/example/daobe/common/config/RedisConfig.java
@@ -0,0 +1,38 @@
+package com.example.daobe.common.config;
+
+import com.example.daobe.auth.domain.Token;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+
+@Configuration
+@EnableRedisRepositories
+public class RedisConfig {
+
+    private final String host;
+    private final int port;
+
+    public RedisConfig(
+            @Value("${spring.data.redis.host}") String host,
+            @Value("${spring.data.redis.port}") int port
+    ) {
+        this.host = host;
+        this.port = port;
+    }
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public RedisTemplate<String, Token> redisTemplate() {
+        RedisTemplate<String, Token> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        return redisTemplate;
+    }
+}

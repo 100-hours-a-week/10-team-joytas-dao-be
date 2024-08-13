a2539dc1f6dbd159cd362de5497a4bdf58b3443a
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-13 23:03:04 +0900

    feat: 카카오 아이디로 사용자 조히 기능 추가

--- src/main/java/com/example/daobe/user/repository/UserRepository.java
+++ src/main/java/com/example/daobe/user/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package com.example.daobe.user.repository;
 
 import com.example.daobe.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByKakaoId(String kakao);
 }

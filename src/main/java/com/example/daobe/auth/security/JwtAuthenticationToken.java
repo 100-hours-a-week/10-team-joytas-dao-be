97ba005b572bc559c3503dff7d5670d922e02cd0
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-13 22:02:11 +0900

    feat: 스프링 시큐리티 JWT 필터 및 발급 로직 추가

---
+++ src/main/java/com/example/daobe/auth/security/JwtAuthenticationToken.java
@@ -0,0 +1,37 @@
+package com.example.daobe.auth.security;
+
+import java.util.Collection;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+public class JwtAuthenticationToken extends UsernamePasswordAuthenticationToken {
+
+    public JwtAuthenticationToken(
+            Object principal,
+            Object credentials
+    ) {
+        super(principal, credentials);
+    }
+
+    public JwtAuthenticationToken(
+            Object principal,
+            Object credentials,
+            Collection<? extends GrantedAuthority> authorities
+    ) {
+        super(principal, credentials, authorities);
+    }
+
+    // 인증 전의 토큰 객체 생성
+    public static JwtAuthenticationToken beforeOf(String accessToken) {
+        return new JwtAuthenticationToken(accessToken, "");
+    }
+
+    // 인증 후의 토큰 객체 생성
+    public static JwtAuthenticationToken afterOf(Long memberId) {
+        return new JwtAuthenticationToken(memberId, "", null);
+    }
+
+    public String getAccessToken() {
+        return (String) this.getPrincipal();
+    }
+}

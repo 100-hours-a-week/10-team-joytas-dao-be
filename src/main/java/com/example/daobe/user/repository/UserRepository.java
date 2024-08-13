ce4365f2a7ac14558c0bf65b5a0f2ff150dbfcbb
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-13 14:30:16 +0900

    rename: UserRepository 패키지명 오타 수정

--- src/main/java/com/example/daobe/user/repository/UserRepository.java
+++ src/main/java/com/example/daobe/user/repository/UserRepository.java
@@ -1,4 +1,4 @@
-package com.example.daobe.user.respository;
+package com.example.daobe.user.repository;
 
 import com.example.daobe.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;

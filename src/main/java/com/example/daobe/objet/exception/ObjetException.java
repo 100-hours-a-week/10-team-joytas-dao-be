07b6e391b92d70e5e05575c5b56d95943a9b6300
jjikky <kok5417@skuniv.ac.kr>
2024-08-13 23:32:55 +0900

    feat : 오브제 예외 처리 파일 추가

---
+++ src/main/java/com/example/daobe/objet/exception/ObjetException.java
@@ -0,0 +1,11 @@
+package com.example.daobe.objet.exception;
+
+import com.example.daobe.common.exception.BaseException;
+import com.example.daobe.common.exception.BaseExceptionType;
+
+public class ObjetException extends BaseException {
+
+    public ObjetException(BaseExceptionType baseExceptionType) {
+        super(baseExceptionType);
+    }
+}

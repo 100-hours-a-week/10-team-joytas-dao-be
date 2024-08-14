99fe2888207cd91f81bcbc5dad5bbe413cbfce64
jjikky <kok5417@skuniv.ac.kr>
2024-08-14 15:33:04 +0900

    feat: 오브제 목록 조회 DTO 추가

--- src/main/java/com/example/daobe/objet/dto/ObjetInfoDto.java
+++ src/main/java/com/example/daobe/objet/dto/ObjetInfoDto.java
@@ -1,4 +1,21 @@
 package com.example.daobe.objet.dto;
 
-public record ObjetInfoDto() {
+import com.example.daobe.objet.entity.Objet;
+import com.example.daobe.objet.entity.ObjetType;
+import lombok.Builder;
+
+@Builder
+public record ObjetInfoDto(
+        Long objetId,
+        ObjetType type,
+        String name
+) {
+    public static ObjetInfoDto of(Objet objet) {
+        return ObjetInfoDto.builder()
+                .objetId(objet.getObjetId())
+                .type(objet.getType())
+                .name(objet.getName())
+                .build();
+    }
+
 }

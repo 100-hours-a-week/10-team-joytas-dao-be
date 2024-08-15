1ff4c5ecf96a97f8eb2fa3857471d691d4d5fb1f
jjikky <kok5417@skuniv.ac.kr>
2024-08-15 18:40:26 +0900

    feat: 오브제 수정 기능 추가

--- src/main/java/com/example/daobe/objet/controller/ObjetController.java
+++ src/main/java/com/example/daobe/objet/controller/ObjetController.java
@@ -5,14 +5,17 @@ import com.example.daobe.objet.dto.ObjetCreateRequestDto;
 import com.example.daobe.objet.dto.ObjetCreateResponseDto;
 import com.example.daobe.objet.dto.ObjetDetailInfoDto;
 import com.example.daobe.objet.dto.ObjetInfoDto;
+import com.example.daobe.objet.dto.ObjetUpdateRequestDto;
 import com.example.daobe.objet.service.ObjetService;
 import com.example.daobe.upload.application.UploadService;
 import com.example.daobe.upload.application.dto.UploadImageResponse;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,9 +26,11 @@ import org.springframework.web.multipart.MultipartFile;
 @RestController
 @RequestMapping("/api/v1/objets")
 @RequiredArgsConstructor
+@Slf4j
 public class ObjetController {
 
     private static final String OBJET_CREATED_SUCCESS = "OBJET_CREATED_SUCCESS";
+    private static final String OBJET_UPDATED_SUCCESS = "OBJET_UPDATED_SUCCESS";
 
     private final ObjetService objetService;
     private final UploadService uploadService;
@@ -33,10 +38,10 @@ public class ObjetController {
     @PostMapping
     public ResponseEntity<ApiResponse<ObjetCreateResponseDto>> generateObjet(
             @RequestParam("name") String name,
-            @RequestParam("description") String description,
             @RequestParam("type") String type,
             @RequestParam("lounge_id") Long loungeId,
             @RequestParam("owners") List<Long> owners,
+            @RequestParam("description") String description,
             @RequestParam("objet_image") MultipartFile file
     ) {
         UploadImageResponse uploadImageResponse = uploadService.uploadImage(file);
@@ -44,6 +49,7 @@ public class ObjetController {
         ObjetCreateRequestDto request = new ObjetCreateRequestDto(owners, name, description, type, loungeId);
 
         ObjetCreateResponseDto ObjetCreateResponse = objetService.create(request, uploadImageResponse.image());
+
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(new ApiResponse<>(OBJET_CREATED_SUCCESS, ObjetCreateResponse));
     }
@@ -66,5 +72,28 @@ public class ObjetController {
                 "OBJET_DETAIL_LOADED_SUCCESS", objetService.getObjetDetail(objetId)
         );
         return ResponseEntity.status(HttpStatus.OK).body(response);
+    }
+
+    @PatchMapping("/{objetId}")
+    public ResponseEntity<ApiResponse<ObjetCreateResponseDto>> updateObjet(
+            @PathVariable(name = "objetId") Long objetId,
+            @RequestParam("name") String name,
+            @RequestParam("owners") List<Long> owners,
+            @RequestParam("description") String description,
+            @RequestParam(value = "objet_image", required = false) MultipartFile file
+    ) {
+        ObjetUpdateRequestDto request = new ObjetUpdateRequestDto(objetId, owners, name, description);
+
+        ObjetCreateResponseDto ObjetUpdateResponse;
+
+        if (file != null && !file.isEmpty()) {
+            UploadImageResponse uploadImageResponse = uploadService.uploadImage(file);
+            ObjetUpdateResponse = objetService.updateWithFile(request, uploadImageResponse.image());
+        } else {
+            ObjetUpdateResponse = objetService.update(request);
+        }
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse<>(OBJET_UPDATED_SUCCESS, ObjetUpdateResponse));
     }
 }

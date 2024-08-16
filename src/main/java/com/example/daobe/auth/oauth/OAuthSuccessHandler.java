ab25d7a76a5ad071e9ebe0d8c1462fb7792d1575
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-16 15:35:06 +0900

    feat: 랜덤 닉네임 생성 로직 추가 #40

--- src/main/java/com/example/daobe/auth/oauth/OAuthSuccessHandler.java
+++ src/main/java/com/example/daobe/auth/oauth/OAuthSuccessHandler.java
@@ -12,7 +12,7 @@ import java.io.IOException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseCookie;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
@@ -41,8 +41,8 @@ public class OAuthSuccessHandler implements AuthenticationSuccessHandler {
             HttpServletResponse response,
             Authentication authentication
     ) throws IOException, ServletException {
-        DefaultOAuth2User oAuth2User = (DefaultOAuth2User) authentication.getPrincipal();
-        TokenResponseDto tokenResponseDto = authService.generateTokenPair(oAuth2User.getName());
+        OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+        TokenResponseDto tokenResponseDto = authService.loginOrRegister(oAuth2User.getName());
         ResponseCookie cookie = cookieHandler.createCookie(
                 REFRESH_TOKEN,
                 tokenResponseDto.refreshToken()

adde364cf0527c19f85536167ba492dc4cddff42
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-16 22:35:52 +0900

    refactor: 라운지 예외 메시지 리팩토링

--- src/main/java/com/example/daobe/lounge/exception/LoungeExceptionType.java
+++ src/main/java/com/example/daobe/lounge/exception/LoungeExceptionType.java
@@ -5,6 +5,10 @@ import org.springframework.http.HttpStatus;
 
 public enum LoungeExceptionType implements BaseExceptionType {
     INVALID_LOUNGE_ID_EXCEPTION("유효하지 않은 라운지 ID입니다.", HttpStatus.NOT_FOUND),
+    NOT_ACTIVE_LOUNGE_EXCEPTION("활성 상태가 아닌 라운지입니다.", HttpStatus.BAD_REQUEST),
+    INVALID_LOUNGE_OWNER_EXCEPTION("유효하지 않은 라운지 주인입니다.", HttpStatus.UNAUTHORIZED),
+    ALREADY_INVITED_USER_EXCEPTION("이미 라운지에 소속된 유저입니다.", HttpStatus.BAD_REQUEST),
+    ALREADY_DELETED_LOUNGE_EXCEPTION("이미 삭제된 라운지입니다.", HttpStatus.BAD_REQUEST),
     ;
 
     private String message;

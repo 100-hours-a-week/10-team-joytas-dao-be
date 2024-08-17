ea80de0b77d3eafd4ae74738b2fe7e1269a2f123
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-18 05:36:38 +0900

    feat: 식별자 기반의 사용자 정보 조회 API 추가 #45

--- src/main/java/com/example/daobe/user/presentation/UserController.java
+++ src/main/java/com/example/daobe/user/presentation/UserController.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +24,16 @@ public class UserController {
     ) {
         return ResponseEntity.ok(new ApiResponse<>(
                 "CURRENT_USER_INFO_LOADED_SUCCESS",
+                userService.getUserInfoWithId(userId)
+        ));
+    }
+
+    @GetMapping("/{userId}")
+    public ResponseEntity<ApiResponse<UserInfoResponseDto>> getUserInfo(
+            @PathVariable Long userId
+    ) {
+        return ResponseEntity.ok(new ApiResponse<>(
+                "LOAD_USER_INFO_SUCCESS",
                 userService.getUserInfoWithId(userId)
         ));
     }

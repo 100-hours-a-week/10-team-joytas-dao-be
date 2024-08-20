fc4f445cd175aa4ab2e85a72accb03dc3d54315b
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-21 01:10:24 +0900

    refactor: 사용자 정보 조회 응답 형식 수정

--- src/main/java/com/example/daobe/user/application/dto/UserInfoResponseDto.java
+++ src/main/java/com/example/daobe/user/application/dto/UserInfoResponseDto.java
@@ -1,9 +1,11 @@
 package com.example.daobe.user.application.dto;
 
 import com.example.daobe.user.domain.User;
+import com.example.daobe.user.domain.UserStatus;
 
 public record UserInfoResponseDto(
         Long userId,
+        UserStatus userStatus,
         String nickname,
         String profileUrl
 ) {
@@ -11,6 +13,7 @@ public record UserInfoResponseDto(
     public static UserInfoResponseDto of(User user) {
         return new UserInfoResponseDto(
                 user.getId(),
+                user.getStatus(),
                 user.getNickname(),
                 user.getProfileUrl()
         );

c89e5492ec7568b7f92e43e9b2f3a4346cadc5ee
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-22 16:42:23 +0900

    fix: 로깅 처리할 미디어 타입 지정 #81

--- src/main/java/com/example/daobe/common/logging/ReqResLoggingFilter.java
+++ src/main/java/com/example/daobe/common/logging/ReqResLoggingFilter.java
@@ -1,5 +1,7 @@
 package com.example.daobe.common.logging;
 
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+
 import com.example.daobe.common.utils.DaoStreamUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -30,6 +32,19 @@ public class ReqResLoggingFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain
     ) throws ServletException, IOException {
+        String contentType = request.getHeader(CONTENT_TYPE);
+        if (LoggingMediaType.isMatchType(contentType)) {
+            doFilterWithCaching(request, response, filterChain);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+
+    private void doFilterWithCaching(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws ServletException, IOException {
         ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper cachedResponse = new ContentCachingResponseWrapper(response);
 
@@ -40,11 +55,9 @@ public class ReqResLoggingFilter extends OncePerRequestFilter {
         filterChain.doFilter(cachedRequest, cachedResponse);
 
         stopWatch.stop();
-        long requestDuration = stopWatch.getTotalTimeMillis();
-
-        logging(cachedRequest, cachedResponse, requestDuration);
-
+        logging(cachedRequest, cachedResponse, stopWatch.getTotalTimeMillis());
         cachedResponse.copyBodyToResponse();
+
     }
 
     // TODO: 리팩토링

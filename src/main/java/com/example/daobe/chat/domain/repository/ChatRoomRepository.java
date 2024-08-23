e6328c22f1833958eaa865a9fd94dccd6f4f4de9
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-23 21:18:53 +0900

    feat: 채팅방 도메인 구현

--- src/main/java/com/example/daobe/chat/domain/repository/ChatRoomRepository.java
+++ src/main/java/com/example/daobe/chat/domain/repository/ChatRoomRepository.java
@@ -1,2 +1,17 @@
-package com.example.daobe.chat.domain.repository;public interface ChatRoomRepository {
+package com.example.daobe.chat.domain.repository;
+
+import com.example.daobe.chat.domain.ChatRoom;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    @Query("""
+            SELECT cr FROM ChatRoom cr
+            LEFT JOIN Objet o ON o.chatRoom.id = cr.id
+            WHERE o.objetId = :objetId
+            """)
+    Optional<ChatRoom> findChatRoomTokenByObjetId(@Param("objetId") Long objetId);
 }

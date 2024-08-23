5c1b7c3235feed5c37944e3313790ebf7c720ada
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-23 13:11:20 +0900

    feature: 라운지 초대시 도메인 이벤트 발행 로직 추가 #84

--- src/main/java/com/example/daobe/lounge/application/LoungeFacadeService.java
+++ src/main/java/com/example/daobe/lounge/application/LoungeFacadeService.java
@@ -12,18 +12,17 @@ import com.example.daobe.lounge.application.dto.LoungeInviteDto;
 import com.example.daobe.lounge.domain.Lounge;
 import com.example.daobe.lounge.domain.LoungeResult;
 import com.example.daobe.lounge.domain.LoungeSharer;
+import com.example.daobe.lounge.domain.event.LoungeInviteEvent;
 import com.example.daobe.lounge.domain.repository.LoungeSharerRepository;
 import com.example.daobe.lounge.exception.LoungeException;
-import com.example.daobe.objet.domain.Objet;
-import com.example.daobe.objet.domain.ObjetSharer;
 import com.example.daobe.objet.domain.repository.ObjetRepository;
-import com.example.daobe.objet.domain.repository.ObjetSharerRepository;
 import com.example.daobe.user.domain.User;
 import com.example.daobe.user.domain.repository.UserRepository;
 import com.example.daobe.user.exception.UserException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -37,6 +36,7 @@ public class LoungeFacadeService {
     private final LoungeService loungeService;
     private final LoungeSharerRepository loungeSharerRepository;
     private final ObjetRepository objetRepository;
+    private final ApplicationEventPublisher eventPublisher;
 
     @Transactional
     public LoungeCreateResponseDto create(LoungeCreateRequestDto request, Long userId) {
@@ -68,18 +68,19 @@ public class LoungeFacadeService {
     }
 
     @Transactional
-    public LoungeResult inviteUser(LoungeInviteDto request) {
+    public LoungeResult inviteUser(Long userId, LoungeInviteDto request) {
         User findUser = findUserById(request.userId());
         Lounge findLounge = findLoungeById(request.loungeId());
 
         // TODO: 알림 기능 구현 이후 유저가 초대 수락시 해당 메서드 실행
         // 해당 라운지에 이미 소속되어 있는지 확인
         if (isNotExistUserInLounge(findUser, findLounge)) {
-            LoungeSharer userLounge = LoungeSharer.builder()
+            LoungeSharer loungeSharer = LoungeSharer.builder()
                     .user(findUser)
                     .lounge(findLounge)
                     .build();
-            loungeSharerRepository.save(userLounge);
+            loungeSharerRepository.save(loungeSharer);
+            eventPublisher.publishEvent(new LoungeInviteEvent(userId, loungeSharer));
             return LoungeResult.LOUNGE_INVITE_SUCCESS;
         }
         throw new LoungeException(ALREADY_INVITED_USER_EXCEPTION);

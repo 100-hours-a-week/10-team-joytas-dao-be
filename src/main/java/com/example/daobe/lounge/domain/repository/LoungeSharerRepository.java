51c6f750765bfdab10348ba57e54b2e9090eaaca
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-28 14:03:42 +0900

    feat: 라운지내 소속된 사용자를 불러오는 로직 추가 #121

--- src/main/java/com/example/daobe/lounge/domain/repository/LoungeSharerRepository.java
+++ src/main/java/com/example/daobe/lounge/domain/repository/LoungeSharerRepository.java
@@ -1,6 +1,7 @@
 package com.example.daobe.lounge.domain.repository;
 
 import com.example.daobe.lounge.domain.LoungeSharer;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,4 +12,6 @@ public interface LoungeSharerRepository extends JpaRepository<LoungeSharer, Long
             + "FROM LoungeSharer ls WHERE ls.user.id = :userId "
             + "AND ls.lounge.id = :loungeId")
     boolean existsByUserIdAndLoungeId(@Param("userId") Long userId, @Param("loungeId") Long loungeId);
+
+    List<LoungeSharer> findByLounge_IdAndUser_NicknameContaining(Long loungeId, String nickname);
 }

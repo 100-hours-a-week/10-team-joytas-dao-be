2d2ed77e488f7c54223d5e9abf1106f22863955c
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-28 15:06:53 +0900

    refactor: SSE 알림 메시지 페이로드 수정

--- src/main/java/com/example/daobe/notification/application/dto/NotificationPayload.java
+++ src/main/java/com/example/daobe/notification/application/dto/NotificationPayload.java
@@ -1,15 +1,18 @@
 package com.example.daobe.notification.application.dto;
 
 import com.example.daobe.common.domain.DomainEvent;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record NotificationPayload(
-        Long receiveUserId,
-        Long sendUserId,
-        String eventType
+        @JsonProperty("type_id") Long typeId,
+        @JsonProperty("receive_user_id") Long receiveUserId,
+        @JsonProperty("send_user_id") Long sendUserId,
+        @JsonProperty("event_type") String eventType
 ) {
 
     public static NotificationPayload of(DomainEvent domainEvent, String eventType) {
         return new NotificationPayload(
+                domainEvent.getDomainId(),
                 domainEvent.getReceiveUserId(),
                 domainEvent.getSendUserId(),
                 eventType

0d5d53ba8126a6872a9d4174afa89cadfcfdfa13
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-28 17:26:39 +0900

    feat: 알림 응답에 알림 발생일 추가 #129

--- src/main/java/com/example/daobe/notification/application/dto/NotificationInfoResponseDto.java
+++ src/main/java/com/example/daobe/notification/application/dto/NotificationInfoResponseDto.java
@@ -3,13 +3,15 @@ package com.example.daobe.notification.application.dto;
 import com.example.daobe.notification.domain.Notification;
 import com.example.daobe.notification.domain.convert.dto.DomainInfo;
 import com.example.daobe.user.domain.User;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 public record NotificationInfoResponseDto(
-        Long notificationId,
-        String type,
-        boolean isRead,
-        UserInfo sender,
-        DomainInfo detail
+        @JsonProperty("notification_id") Long notificationId,
+        @JsonProperty("type") String type,
+        @JsonProperty("is_read") boolean isRead,
+        @JsonProperty("datetime") String dateTime,
+        @JsonProperty("sender") UserInfo sender,
+        @JsonProperty("detail") DomainInfo detail
 ) {
 
     public static NotificationInfoResponseDto of(Notification notification, DomainInfo domainInfo) {
@@ -17,6 +19,7 @@ public record NotificationInfoResponseDto(
                 notification.getId(),
                 notification.getType().type(),
                 notification.isRead(),
+                notification.getCreatedAt().toString(),
                 UserInfo.of(notification.getSendUser()),
                 domainInfo
         );

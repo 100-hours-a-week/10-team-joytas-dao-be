70ac9a32d4c61eda798cda3f9307c831448c3ba3
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-28 15:02:11 +0900

    refactor: 알림 엔티티 필드 수정 및 예외 타입 추가

--- src/main/java/com/example/daobe/notification/domain/Notification.java
+++ src/main/java/com/example/daobe/notification/domain/Notification.java
@@ -1,9 +1,14 @@
 package com.example.daobe.notification.domain;
 
+import static com.example.daobe.notification.exception.NotificationExceptionType.IS_NOT_OWN_NOTIFICATION;
+
 import com.example.daobe.common.domain.BaseTimeEntity;
+import com.example.daobe.notification.exception.NotificationException;
 import com.example.daobe.user.domain.User;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,7 +16,9 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import java.util.Objects;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -30,9 +37,27 @@ public class Notification extends BaseTimeEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     private User user;
 
-    // 알림 타입 ex) N0001, N0002
-    private String type;
+    @Column(name = "domain_id")
+    private Long domainId;
+
+    @Enumerated(value = EnumType.STRING)
+    private NotificationEventType type;
 
     @Column(name = "is_read")
     private boolean isRead;
+
+    @Builder
+    public Notification(User user, Long domainId, NotificationEventType type) {
+        this.user = user;
+        this.domainId = domainId;
+        this.type = type;
+        this.isRead = false;
+    }
+
+    public void updateReadStateIfOwnNotification(Long userId) {
+        if (Objects.equals(user.getId(), userId)) {
+            isRead = true;
+        }
+        throw new NotificationException(IS_NOT_OWN_NOTIFICATION);
+    }
 }

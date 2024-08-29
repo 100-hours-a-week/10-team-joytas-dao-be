54c4f1da5d797772b66a30e1f84f1b0e73002551
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-29 16:02:50 +0900

    feat: 유저별 활성화된 라운지 중 소속된 라운지 개수 반환 쿼리 추가 #137

--- src/main/java/com/example/daobe/lounge/domain/repository/LoungeSharerRepository.java
+++ src/main/java/com/example/daobe/lounge/domain/repository/LoungeSharerRepository.java
@@ -14,4 +14,11 @@ public interface LoungeSharerRepository extends JpaRepository<LoungeSharer, Long
     boolean existsByUserIdAndLoungeId(@Param("userId") Long userId, @Param("loungeId") Long loungeId);
 
     List<LoungeSharer> findByLounge_IdAndUser_NicknameContaining(Long loungeId, String nickname);
+
+    @Query("""
+                SELECT COUNT(ls) FROM LoungeSharer ls
+                JOIN ls.lounge l WHERE l.status = 'ACTIVE'
+                AND ls.user.id = :userId
+            """)
+    long countActiveLoungeSharerByUserId(@Param("userId") Long userId);
 }

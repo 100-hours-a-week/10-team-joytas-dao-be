11934355856962ad31091c1d4ebf6676828df82c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-08-29 13:41:12 +0900

    refactor: LoungeFacadeService 구조 변경 및 Service만 의존하도록 리팩토링 #132

--- src/main/java/com/example/daobe/lounge/application/LoungeFacadeService.java
+++ src/main/java/com/example/daobe/lounge/application/LoungeFacadeService.java
@@ -1,109 +1,69 @@
 package com.example.daobe.lounge.application;
 
-import static com.example.daobe.lounge.exception.LoungeExceptionType.ALREADY_INVITED_USER_EXCEPTION;
-import static com.example.daobe.objet.domain.ObjetStatus.ACTIVE;
-import static com.example.daobe.user.exception.UserExceptionType.NOT_EXIST_USER;
-
 import com.example.daobe.lounge.application.dto.LoungeCreateRequestDto;
 import com.example.daobe.lounge.application.dto.LoungeCreateResponseDto;
 import com.example.daobe.lounge.application.dto.LoungeDetailInfoDto;
-import com.example.daobe.lounge.application.dto.LoungeDetailInfoDto.ObjetInfo;
 import com.example.daobe.lounge.application.dto.LoungeInfoDto;
 import com.example.daobe.lounge.application.dto.LoungeInviteDto;
 import com.example.daobe.lounge.domain.Lounge;
-import com.example.daobe.lounge.domain.LoungeResult;
 import com.example.daobe.lounge.domain.LoungeSharer;
 import com.example.daobe.lounge.domain.event.LoungeInviteEvent;
-import com.example.daobe.lounge.domain.repository.LoungeSharerRepository;
-import com.example.daobe.lounge.exception.LoungeException;
+import com.example.daobe.user.application.UserService;
 import com.example.daobe.user.domain.User;
-import com.example.daobe.user.domain.repository.UserRepository;
-import com.example.daobe.user.exception.UserException;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Slf4j
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class LoungeFacadeService {
 
-    private final UserRepository userRepository;
+    private final UserService userService;
     private final LoungeService loungeService;
-    private final LoungeSharerRepository loungeSharerRepository;
+    private final LoungeSharerService loungeSharerService;
     private final ApplicationEventPublisher eventPublisher;
 
+    // 라운지 생성
     @Transactional
-    public LoungeCreateResponseDto create(LoungeCreateRequestDto request, Long userId) {
-        User findUser = findUserById(userId);
-        LoungeCreateResponseDto response = loungeService.createLounge(request, findUser);
-        Lounge findLounge = loungeService.findLoungeById(response.loungeId());
-
-        LoungeSharer userLounge = LoungeSharer.builder()
-                .user(findUser)
-                .lounge(findLounge)
-                .build();
-        loungeSharerRepository.save(userLounge);
-        return LoungeCreateResponseDto.of(findLounge);
+    public LoungeCreateResponseDto createLounge(LoungeCreateRequestDto request, Long userId) {
+        User findUser = userService.getUserById(userId);
+        Lounge createdLounge = loungeService.createAndSaveLounge(request, findUser);
+        loungeSharerService.createAndSaveLoungeSharer(findUser, createdLounge);
+        return LoungeCreateResponseDto.of(createdLounge);
     }
 
+    // 라운지 목록 조회
+    public List<LoungeInfoDto> getAllLounges(Long userId) {
+        return loungeService.getLoungeInfosByUserId(userId);
+    }
+
+    // 라운지 상세 조회
     public LoungeDetailInfoDto getLoungeDetail(Long userId, Long loungeId) {
-        Lounge findLounge = findLoungeById(loungeId);
-        findLounge.validate(userId);
-        List<LoungeDetailInfoDto.ObjetInfo> objetInfos = findLounge.getObjets()
-                .stream()
-                .filter(objet -> objet.getStatus() == ACTIVE)
-                .map(ObjetInfo::of)
-                .toList();
-
-        return loungeService.createLoungeDetailInfo(loungeId, objetInfos);
+        Lounge findLounge = loungeService.getLoungeById(loungeId);
+        return loungeService.getLoungeDetailInfo(userId, findLounge);
     }
 
+    // 라운지 삭제
     @Transactional
-    public LoungeResult inviteUser(Long userId, LoungeInviteDto request) {
-        User findUser = findUserById(request.userId());
-        Lounge findLounge = findLoungeById(request.loungeId());
+    public void deleteLounge(Long userId, Long loungeId) {
+        User findUser = userService.getUserById(userId);
+        Lounge findLounge = loungeService.getLoungeById(loungeId);
+        loungeService.deleteLoungeByUserId(findUser, findLounge);
+    }
 
-        // TODO: 알림 기능 구현 이후 유저가 초대 수락시 해당 메서드 실행
-        // 해당 라운지에 이미 소속되어 있는지 확인
-        if (isNotExistUserInLounge(findUser, findLounge)) {
-            LoungeSharer loungeSharer = LoungeSharer.builder()
-                    .user(findUser)
-                    .lounge(findLounge)
-                    .build();
-            loungeSharerRepository.save(loungeSharer);
-            eventPublisher.publishEvent(new LoungeInviteEvent(userId, loungeSharer));
-            return LoungeResult.LOUNGE_INVITE_SUCCESS;
+    // 라운지 초대
+    @Transactional
+    public void inviteUser(LoungeInviteDto request, Long inviterId) {
+        User findUser = userService.getUserById(request.userId());
+        Lounge findLounge = loungeService.getLoungeById(request.loungeId());
+        LoungeSharer loungeSharer = loungeSharerService.inviteUser(findUser, findLounge, inviterId);
+
+        // 초대 성공시 이벤트 발행
+        if (loungeSharer != null) {
+            eventPublisher.publishEvent(new LoungeInviteEvent(inviterId, loungeSharer));
         }
-        throw new LoungeException(ALREADY_INVITED_USER_EXCEPTION);
-    }
-
-    @Transactional
-    public void delete(Long userId, Long loungeId) {
-        User findUser = findUserById(userId);
-        Lounge findLounge = findLoungeById(loungeId);
-        findLounge.softDelete(findUser.getId());
-    }
-
-    // find
-    public List<LoungeInfoDto> findLoungeByUserId(Long userId) {
-        return loungeService.findLoungeByUserId(userId);
-    }
-
-    private Lounge findLoungeById(Long id) {
-        return loungeService.findLoungeById(id);
-    }
-
-    private User findUserById(Long id) {
-        return userRepository.findById(id)
-                .orElseThrow(() -> new UserException(NOT_EXIST_USER));
-    }
-
-    private boolean isNotExistUserInLounge(User user, Lounge lounge) {
-        return !loungeSharerRepository.existsByUserIdAndLoungeId(user.getId(), lounge.getId());
     }
 }

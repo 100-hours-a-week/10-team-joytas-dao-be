2ef511a9ac0d48bf22ae46831bdf9fe242962578
JiHongKim98 <kinjihong9598@gmail.com>
2024-08-30 19:57:30 +0900

    feat: 콕 찌르기 예외 타입 추가 #155

--- src/main/java/com/example/daobe/user/exception/UserExceptionType.java
+++ src/main/java/com/example/daobe/user/exception/UserExceptionType.java
@@ -6,7 +6,9 @@ import org.springframework.http.HttpStatus;
 public enum UserExceptionType implements BaseExceptionType {
     NOT_EXIST_USER("존재하지 않는 사용자입니다", HttpStatus.NOT_FOUND),
     DUPLICATE_NICKNAME("이미 사용 중인 닉네임입니다.", HttpStatus.CONFLICT),
-    NOT_MATCH_REASON_TYPE("일치하는 사용자 상태가 없습니다.", HttpStatus.BAD_REQUEST);
+    ALREADY_POKE("콕 찌르기는 3시간에 한번 가능합니다.", HttpStatus.BAD_REQUEST),
+    NOT_MATCH_REASON_TYPE("일치하는 사유 타입이 없습니다.", HttpStatus.BAD_REQUEST),
+    ;
 
     private final String message;
     private final HttpStatus status;

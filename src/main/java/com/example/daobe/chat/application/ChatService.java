e060b730335ca7fb6c9e3aa5318608313a86560f
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-09-01 17:24:38 +0900

    refactor: ChatService, ChatRoomService 통합 및 하드코딩 상수화 #161

--- src/main/java/com/example/daobe/chat/application/ChatService.java
+++ src/main/java/com/example/daobe/chat/application/ChatService.java
@@ -3,13 +3,17 @@ package com.example.daobe.chat.application;
 import static com.example.daobe.chat.domain.MessageType.ENTER;
 import static com.example.daobe.chat.domain.MessageType.LEAVE;
 import static com.example.daobe.chat.domain.MessageType.TALK;
+import static com.example.daobe.chat.exception.ChatExceptionType.INVALID_CHAT_ROOM_ID_EXCEPTION;
+import static com.example.daobe.chat.exception.ChatExceptionType.SOCKET_CONNECTION_FAILED_EXCEPTION;
 
 import com.example.daobe.chat.application.dto.ChatMessageDto;
+import com.example.daobe.chat.application.dto.ChatMessageDto.EnterAndLeaveMessage;
 import com.example.daobe.chat.application.dto.ChatRoomTokenDto;
 import com.example.daobe.chat.domain.ChatMessage;
 import com.example.daobe.chat.domain.ChatRoom;
 import com.example.daobe.chat.domain.repository.ChatMessageRepository;
 import com.example.daobe.chat.domain.repository.ChatRoomRepository;
+import com.example.daobe.chat.exception.ChatException;
 import com.example.daobe.user.application.UserService;
 import com.example.daobe.user.application.dto.UserInfoResponseDto;
 import java.time.LocalDateTime;
@@ -29,41 +33,46 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 public class ChatService {
 
-    private final ChatMessageRepository chatMessageRepository;
-    private final ChatRoomRepository chatRoomRepository;
+    private static final String SENDER_ID = "senderId";
+    private static final String SENDER_NAME = "senderName";
+    private static final String ROOM_TOKEN = "roomToken";
+    private static final int RECENT_MESSAGES_COUNT = 3;
+
     private final UserService userService;
+    private final ChatRoomRepository chatRoomRepository;
+    private final ChatMessageRepository chatMessageRepository;
+
+    @Transactional
+    public ChatRoom createChatRoom() {
+        return chatRoomRepository.save(new ChatRoom());
+    }
 
     public ChatRoomTokenDto getRoomTokenByObjetId(Long objetId) {
         // TODO: 사용자 인증, 오브제 존재 여부 등 검증
         ChatRoom findChatRoom = chatRoomRepository.findChatRoomTokenByObjetId(objetId)
-                .orElseThrow(() -> new RuntimeException("존재하지 않는 채팅방"));
+                .orElseThrow(() -> new ChatException(INVALID_CHAT_ROOM_ID_EXCEPTION));
         return new ChatRoomTokenDto(findChatRoom.getRoomToken());
     }
 
-    public ChatMessageDto.EnterAndLeaveMessage createEnterMessageAndSetSessionAttribute(
+    public EnterAndLeaveMessage createEnterMessageAndSetSessionAttribute(
             ChatMessageDto message,
             String roomToken,
             SimpMessageHeaderAccessor headerAccessor
     ) {
         try {
-            Objects.requireNonNull(headerAccessor.getSessionAttributes()).put("senderId", message.senderId());
-            headerAccessor.getSessionAttributes().put("senderName", message.senderName());
-            headerAccessor.getSessionAttributes().put("roomToken", roomToken);
+            Objects.requireNonNull(headerAccessor.getSessionAttributes()).put(SENDER_ID, message.senderId());
+            headerAccessor.getSessionAttributes().put(SENDER_NAME, message.senderName());
+            headerAccessor.getSessionAttributes().put(ROOM_TOKEN, roomToken);
         } catch (Exception e) {
-            throw new RuntimeException("SOCKET_CONNECTION_FAILED_EXCEPTION");
+            throw new ChatException(SOCKET_CONNECTION_FAILED_EXCEPTION);
         }
 
         UserInfoResponseDto findUser = userService.getUserInfoWithId(message.senderId());
-
-        log.info("findUser for enter the chatting-room: {}", findUser.nickname());
-        log.info("headerAccessor - senderId: {}", headerAccessor.getSessionAttributes().get("senderId"));
-        log.info("headerAccessor - senderName: {}", headerAccessor.getSessionAttributes().get("senderName"));
-
-        return new ChatMessageDto.EnterAndLeaveMessage(
+        return EnterAndLeaveMessage.of(
                 ENTER.name(),
                 message.senderId(),
                 roomToken,
-                "%s님이 입장하셨습니다.".formatted(findUser.nickname()),
+                ENTER.getMessage().formatted(findUser.nickname()),
                 LocalDateTime.now()
         );
     }
@@ -83,17 +92,7 @@ public class ChatService {
 
         // MongoDB 저장
         ChatMessage savedChatMessage = chatMessageRepository.save(chatMessage);
-
-        return new ChatMessageDto(
-                savedChatMessage.getId(),
-                savedChatMessage.getType(),
-                savedChatMessage.getRoomToken(),
-                savedChatMessage.getSenderId(),
-                savedChatMessage.getSender(),
-                savedChatMessage.getSenderProfileUrl(),
-                savedChatMessage.getMessage(),
-                savedChatMessage.getCreatedAt()
-        );
+        return ChatMessageDto.of(savedChatMessage);
     }
 
     public List<ChatMessageDto> getMessagesByRoomToken(String roomToken, boolean isAll) {
@@ -103,23 +102,22 @@ public class ChatService {
                     .toList();
         }
         return chatMessageRepository.findAllByRoomTokenOrderByCreatedAtDesc(roomToken).stream()
-                .limit(3)
+                .limit(RECENT_MESSAGES_COUNT)
                 .map(ChatMessageDto::of)
                 .sorted(Comparator.comparing(ChatMessageDto::createdAt))
                 .toList();
     }
 
-    public ChatMessageDto.EnterAndLeaveMessage leaveChatRoom(SimpMessageHeaderAccessor headerAccessor) {
-        String roomToken = (String) headerAccessor.getSessionAttributes().get("roomToken");
-        String senderName = (String) headerAccessor.getSessionAttributes().get("senderName");
-        Long senderId = (Long) headerAccessor.getSessionAttributes().get("senderId");
-        String leaveMessage = senderName + "님이 퇴장하셨습니다.";
+    public EnterAndLeaveMessage leaveChatRoom(SimpMessageHeaderAccessor headerAccessor) {
+        String roomToken = (String) headerAccessor.getSessionAttributes().get(ROOM_TOKEN);
+        String senderName = (String) headerAccessor.getSessionAttributes().get(SENDER_NAME);
+        Long senderId = (Long) headerAccessor.getSessionAttributes().get(SENDER_ID);
 
-        return new ChatMessageDto.EnterAndLeaveMessage(
+        return EnterAndLeaveMessage.of(
                 LEAVE.name(),
                 senderId,
                 roomToken,
-                leaveMessage,
+                LEAVE.getMessage().formatted(senderName),
                 LocalDateTime.now()
         );
     }

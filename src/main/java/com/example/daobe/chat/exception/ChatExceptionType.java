76ccc561f5e702fb07fbb89afe718b1065597ad1
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-09-01 17:27:35 +0900

    rename: ChatExceptionType, ChatRoomExceptionType 통합 및 예외 추가 #161

--- src/main/java/com/example/daobe/chat/exception/ChatExceptionType.java
+++ src/main/java/com/example/daobe/chat/exception/ChatExceptionType.java
@@ -3,14 +3,16 @@ package com.example.daobe.chat.exception;
 import com.example.daobe.common.exception.BaseExceptionType;
 import org.springframework.http.HttpStatus;
 
-public enum ChatRoomExceptionType implements BaseExceptionType {
+public enum ChatExceptionType implements BaseExceptionType {
+
+    SOCKET_CONNECTION_FAILED_EXCEPTION("소켓 연결에 실패했습니다.", HttpStatus.SERVICE_UNAVAILABLE),
     INVALID_CHAT_ROOM_ID_EXCEPTION("유효하지 않은 채팅방 ID입니다.", HttpStatus.NOT_FOUND),
     ;
 
     private final String message;
     private final HttpStatus status;
 
-    ChatRoomExceptionType(String message, HttpStatus status) {
+    ChatExceptionType(String message, HttpStatus status) {
         this.message = message;
         this.status = status;
     }

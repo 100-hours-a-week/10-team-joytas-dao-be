ed94b5a1ad99cfad21a9850d7f379d0d3bb3273b
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-09-11 00:42:42 +0900

    feat: 채팅 메시지 페이징 처리를 위한 메서드 추가 #190

--- src/main/java/com/example/daobe/chat/domain/repository/ChatMessageRepository.java
+++ src/main/java/com/example/daobe/chat/domain/repository/ChatMessageRepository.java
@@ -2,11 +2,18 @@ package com.example.daobe.chat.domain.repository;
 
 import com.example.daobe.chat.domain.ChatMessage;
 import java.util.List;
+import org.bson.types.ObjectId;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
-
-    List<ChatMessage> findAllByRoomToken(String roomToken);
+public interface ChatMessageRepository extends MongoRepository<ChatMessage, ObjectId> {
 
     List<ChatMessage> findAllByRoomTokenOrderByCreatedAtDesc(String roomToken);
+
+    Slice<ChatMessage> findByRoomToken(String roomToken, Pageable pageable);
+
+    // 과거 메시지 (cursorId 이전 메시지)
+    Slice<ChatMessage> findByRoomTokenAndIdLessThan(String roomToken, ObjectId cursorId, Pageable pageable
+    );
 }

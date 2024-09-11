8d3e69baa737abec37a9dc9176140cbf3a084b73
jjikky <kok5417@skuniv.ac.kr>
2024-09-11 21:31:01 +0900

    refactor: 오브제 조회 관련 로직 data jpa -> jpql 변경 #199

--- src/main/java/com/example/daobe/objet/domain/repository/ObjetRepository.java
+++ src/main/java/com/example/daobe/objet/domain/repository/ObjetRepository.java
@@ -1,9 +1,12 @@
 package com.example.daobe.objet.domain.repository;
 
+import com.example.daobe.objet.application.dto.ObjetInfoDto;
 import com.example.daobe.objet.domain.Objet;
 import com.example.daobe.objet.domain.ObjetStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -18,4 +21,28 @@ public interface ObjetRepository extends JpaRepository<Objet, Long> {
     List<Objet> findByLoungeIdAndDeletedAtIsNullAndStatusOrderByIdDesc(
             Long loungeId, ObjetStatus status
     );
+
+    @Query("""
+            SELECT o FROM Objet o 
+            JOIN o.objetSharers s 
+            WHERE o.lounge.id = :loungeId 
+            AND o.deletedAt IS NULL 
+            AND o.status = :status 
+            AND s.user.id = :userId 
+            ORDER BY o.id DESC
+            """)
+    List<ObjetInfoDto> findActiveObjetsInLoungeOfSharer(@Param("userId") Long userId,
+                                                        @Param("loungeId") Long loungeId,
+                                                        @Param("status") ObjetStatus status);
+
+    @Query("""
+            SELECT o FROM Objet o
+            WHERE o.lounge.id = :loungeId
+            AND o.deletedAt IS NULL
+            AND o.status = :status
+            ORDER BY o.id DESC
+            """)
+    List<ObjetInfoDto> findActiveObjetsInLounge(@Param("loungeId") Long loungeId, @Param("status") ObjetStatus status);
+
+
 }

73ce731bbb790f0bcdc02deed6ae43215a9b0787
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-09-11 16:39:04 +0900

    feat: 유효하지 않은 라운지 타입 커스텀 예외 추가 #192

--- src/main/java/com/example/daobe/lounge/exception/LoungeExceptionType.java
+++ src/main/java/com/example/daobe/lounge/exception/LoungeExceptionType.java
@@ -5,13 +5,14 @@ import org.springframework.http.HttpStatus;
 
 public enum LoungeExceptionType implements BaseExceptionType {
     INVALID_LOUNGE_ID_EXCEPTION("유효하지 않은 라운지 ID입니다.", HttpStatus.NOT_FOUND),
+    INVALID_LOUNGE_TYPE_EXCEPTION("유효하지 않은 라운지 타입입니다.", HttpStatus.BAD_REQUEST),
     NOT_ACTIVE_LOUNGE_EXCEPTION("활성 상태가 아닌 라운지입니다.", HttpStatus.NOT_FOUND),
     INVALID_LOUNGE_OWNER_EXCEPTION("유효하지 않은 라운지 주인입니다.", HttpStatus.FORBIDDEN),
     INVALID_LOUNGE_SHARER_EXCEPTION("유효하지 않은 라운지 공유자입니다.", HttpStatus.FORBIDDEN),
-    ALREADY_INVITED_USER_EXCEPTION("이미 라운지에 소속된 유저입니다.", HttpStatus.METHOD_NOT_ALLOWED),
-    ALREADY_DELETED_LOUNGE_EXCEPTION("이미 삭제된 라운지입니다.", HttpStatus.METHOD_NOT_ALLOWED),
+    ALREADY_INVITED_USER_EXCEPTION("이미 라운지에 소속된 유저입니다.", HttpStatus.FORBIDDEN),
+    ALREADY_DELETED_LOUNGE_EXCEPTION("이미 삭제된 라운지입니다.", HttpStatus.FORBIDDEN),
     MAXIMUM_LOUNGE_LIMIT_EXCEEDED_EXCEPTION("라운지 개수는 최대 4개를 초과할 수 없습니다.", HttpStatus.BAD_REQUEST),
-    ;
+    NOT_ALLOW_LOUNGE_WITHDRAW_EXCEPTION("라운지 생성자는 탈퇴할 수 없습니다.", HttpStatus.FORBIDDEN);
 
     private final String message;
     private final HttpStatus status;

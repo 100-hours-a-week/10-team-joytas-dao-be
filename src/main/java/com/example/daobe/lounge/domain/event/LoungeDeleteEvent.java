6b9b72a1c53b655518f0b18fd7389fe42b230f88
jjikky <kok5417@skuniv.ac.kr>
2024-09-16 18:10:05 +0900

    refactor: 라운지 삭제 이벤트 record로 변경

--- src/main/java/com/example/daobe/lounge/domain/event/LoungeDeleteEvent.java
+++ src/main/java/com/example/daobe/lounge/domain/event/LoungeDeleteEvent.java
@@ -1,16 +1,6 @@
 package com.example.daobe.lounge.domain.event;
 
-public class LoungeDeleteEvent {
-
-    private final Long domainId;
-
-    public LoungeDeleteEvent(Long loungeId) {
-        this.domainId = loungeId;
-    }
-
-    public Long getDomainId() {
-        return domainId;
-    }
-
-
+public record LoungeDeleteEvent(
+        Long loungeId
+) {
 }

444870ed159b0fe32f7a87d82951cc15e92cf260
jjikky <kok5417@skuniv.ac.kr>
2024-09-20 15:44:20 +0900

    refactor: 오브제 공유자 관련 로직 수정

--- src/main/java/com/example/daobe/objet/application/ObjetSharerService.java
+++ src/main/java/com/example/daobe/objet/application/ObjetSharerService.java
@@ -1,19 +1,14 @@
 package com.example.daobe.objet.application;
 
-import static com.example.daobe.user.exception.UserExceptionType.NOT_EXIST_USER;
-
 import com.example.daobe.objet.application.dto.ObjetCreateRequestDto;
-import com.example.daobe.objet.application.dto.ObjetUpdateRequestDto;
 import com.example.daobe.objet.domain.Objet;
 import com.example.daobe.objet.domain.ObjetSharer;
 import com.example.daobe.objet.domain.event.ObjetInviteEvent;
 import com.example.daobe.objet.domain.repository.ObjetSharerRepository;
+import com.example.daobe.user.application.UserService;
 import com.example.daobe.user.domain.User;
 import com.example.daobe.user.domain.repository.UserRepository;
-import com.example.daobe.user.exception.UserException;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
@@ -24,63 +19,62 @@ public class ObjetSharerService {
 
     private final ObjetSharerRepository objetSharerRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
     private final ApplicationEventPublisher eventPublisher;
 
-    public void createAndSaveObjetSharer(ObjetCreateRequestDto request, User user, Objet objet) {
-        List<Long> sharerIds = request.sharers();
-        sharerIds.add(user.getId());
+    public List<ObjetSharer> getRecentObjetSharerList(Long userId) {
+        return objetSharerRepository.findTop4ByUserIdOrderByIdDesc(userId);
+    }
 
-        List<ObjetSharer> objetSharers = sharerIds.stream()
-                .map(sharerId -> {
-                    User sharer = getUserById(sharerId);
-                    ObjetSharer newObjetSharer = ObjetSharer.builder()
-                            .user(sharer)
-                            .objet(objet)
-                            .build();
-                    objetSharerRepository.save(newObjetSharer);
-                    eventPublisher.publishEvent(new ObjetInviteEvent(sharer.getId(), newObjetSharer));
-                    return newObjetSharer;
-                })
+    public void createAndSaveObjetSharer(ObjetCreateRequestDto request, Long userId, Objet objet) {
+        List<Long> sharerIdList = request.sharers();
+        sharerIdList.add(userId);
+        List<Long> distinctSharerIds = sharerIdList.stream().distinct().toList();
+
+        List<User> users = userRepository.findAllById(distinctSharerIds);
+
+        List<ObjetSharer> objetSharerList = users.stream()
+                .map(user -> ObjetSharer.builder()
+                        .user(user)
+                        .objet(objet)
+                        .build())
                 .toList();
 
-        objet.updateUserObjets(objetSharers);
+        objetSharerRepository.saveAll(objetSharerList);
+        objetSharerList.forEach(objetSharer ->
+                eventPublisher.publishEvent(new ObjetInviteEvent(userId, objetSharer))
+        );
     }
 
-    public void updateAndSaveObjetSharer(ObjetUpdateRequestDto request, User findUser, Objet findObjet) {
-        Set<Long> currentSharerIds = getCurrentSharerIds(findObjet);
-        List<Long> newSharerIds = request.sharers();
-        newSharerIds.add(findUser.getId());
+    public void updateObjetSharerList(Objet objet, List<Long> sharerIdList) {
+        sharerIdList.add(objet.getUser().getId());
+        List<Long> distinctSharerIds = sharerIdList.stream().distinct().toList();
+        List<Long> currentObjetSharerIdList = objetSharerRepository.findSharerIdsByObjet(objet);
 
-        newSharerIds.stream()
-                .filter(newSharerId -> !currentSharerIds.contains(newSharerId))
-                .forEach(newSharerId -> {
-                    User user = getUserById(newSharerId);
-                    ObjetSharer newObjetSharer = ObjetSharer.builder()
-                            .user(user)
-                            .objet(findObjet)
-                            .build();
-                    objetSharerRepository.save(newObjetSharer);
-                    eventPublisher.publishEvent(new ObjetInviteEvent(findUser.getId(), newObjetSharer));
-                    findObjet.getObjetSharers().add(newObjetSharer);
-                });
+        List<Long> deleteObjetSharerIdList = currentObjetSharerIdList.stream()
+                .filter(sharerId -> !distinctSharerIds.contains(sharerId))
+                .toList();
+        objetSharerRepository.deleteAllById(deleteObjetSharerIdList);
 
-        findObjet.getObjetSharers().removeIf(objetSharer -> {
-            if (!newSharerIds.contains(objetSharer.getUser().getId())) {
-                objetSharerRepository.delete(objetSharer);
-                return true;
-            }
-            return false;
-        });
+        objetSharerRepository.deleteAllByObjetAndUserIdIn(objet, deleteObjetSharerIdList);
+
+        List<ObjetSharer> newObjetSharerList = distinctSharerIds.stream()
+                .filter(sharerId -> !currentObjetSharerIdList.contains(sharerId))
+                .map((sharerId) -> createSharer(sharerId, objet))
+                .toList();
+
+        objetSharerRepository.saveAll(newObjetSharerList);
+        newObjetSharerList.forEach(
+                objetSharer -> eventPublisher.publishEvent(new ObjetInviteEvent(objet.getUser().getId(), objetSharer)));
+
     }
 
-    private Set<Long> getCurrentSharerIds(Objet findObjet) {
-        return findObjet.getObjetSharers().stream()
-                .map(objetSharer -> objetSharer.getUser().getId())
-                .collect(Collectors.toSet());
+    private ObjetSharer createSharer(Long sharerId, Objet objet) {
+        return ObjetSharer.builder()
+                .user(userService.getUserById(sharerId))
+                .objet(objet)
+                .build();
     }
 
-    private User getUserById(Long userId) {
-        return userRepository.findById(userId)
-                .orElseThrow(() -> new UserException(NOT_EXIST_USER));
-    }
 }
+

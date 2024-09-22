5a3f8a2ed4ab501056fb732af34b94684f551c89
JiHongKim98 <kinjihong9598@gmail.com>
2024-09-22 19:33:29 +0900

    remove: 사용하지 않는 예외 타입 삭제 #218

--- src/main/java/com/example/daobe/user/exception/UserExceptionType.java
+++ src/main/java/com/example/daobe/user/exception/UserExceptionType.java
@@ -7,7 +7,6 @@ public enum UserExceptionType implements BaseExceptionType {
     DISABLED_USER("삭제된 사용자입니다.", HttpStatus.NOT_FOUND),
     NOT_EXIST_USER("존재하지 않는 사용자입니다.", HttpStatus.NOT_FOUND),
     DUPLICATE_NICKNAME("이미 사용 중인 닉네임입니다.", HttpStatus.CONFLICT),
-    ALREADY_POKE("콕 찌르기는 3시간에 한번 가능합니다.", HttpStatus.BAD_REQUEST),
     NOT_MATCH_REASON_TYPE("일치하는 사유 타입이 없습니다.", HttpStatus.BAD_REQUEST),
     ;
 

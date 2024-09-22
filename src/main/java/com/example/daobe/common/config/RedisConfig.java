bb95b7f77f177d7c2bae0e18e4edfd0540e22d9a
JiHongKim98 <kinjihong9598@gmail.com>
2024-09-22 23:54:42 +0900

    feat: 레디스 서버 연결 유지 설정 추가 #218

--- src/main/java/com/example/daobe/common/config/RedisConfig.java
+++ src/main/java/com/example/daobe/common/config/RedisConfig.java
@@ -1,7 +1,14 @@
 package com.example.daobe.common.config;
 
 import com.example.daobe.notification.infrastructure.redis.RedisNotificationMessageListener;
+import io.lettuce.core.RedisClient;
+import io.lettuce.core.RedisURI;
+import io.lettuce.core.api.StatefulRedisConnection;
+import io.lettuce.core.codec.ByteArrayCodec;
+import io.lettuce.core.codec.RedisCodec;
+import io.lettuce.core.codec.StringCodec;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,7 +23,17 @@ public class RedisConfig {
 
     private static final String NOTIFICATION_CHANNEL_TOPIC = "notification:channel";
 
+    private final RedisProperties redisProperties;
     private final RedisNotificationMessageListener notificationMessageListener;
+
+    @Bean
+    public StatefulRedisConnection<String, byte[]> redisClient() {
+        RedisURI redisUri = RedisURI.Builder.redis(redisProperties.getHost())
+                .withPort(redisProperties.getPort())
+                .build();
+        RedisClient redisClient = RedisClient.create(redisUri);
+        return redisClient.connect(RedisCodec.of(StringCodec.UTF8, ByteArrayCodec.INSTANCE));
+    }
 
     @Bean
     public RedisMessageListenerContainer notificationContainer(RedisConnectionFactory connectionFactory) {

259d0b1b207e28f78c86b279223e7b2fff51823a
jjikky <kok5417@skuniv.ac.kr>
2024-09-23 09:52:58 +0900

    chore: 오브제 상세 조회 응답 dto owner depth추가 #216

--- src/main/java/com/example/daobe/objet/application/dto/ObjetDetailResponseDto.java
+++ src/main/java/com/example/daobe/objet/application/dto/ObjetDetailResponseDto.java
@@ -6,29 +6,35 @@ import java.time.LocalDateTime;
 
 public record ObjetDetailResponseDto(
         Long objetId,
-        Long userId,
         Long loungeId,
         String name,
-        String nickname,
         String objetImage,
         String description,
         ObjetType objetType,
-        LocalDateTime createdAt
+        LocalDateTime createdAt,
+        OwnerDto owner
 ) {
     public static ObjetDetailResponseDto of(
             Objet objet
     ) {
         return new ObjetDetailResponseDto(
                 objet.getId(),
-                objet.getUser().getId(),
                 objet.getLounge().getId(),
                 objet.getName(),
-                objet.getUser().getNickname(),
                 objet.getImageUrl(),
                 objet.getExplanation(),
                 objet.getType(),
-                objet.getCreatedAt()
+                objet.getCreatedAt(),
+                new OwnerDto(
+                        objet.getUser().getId(),
+                        objet.getUser().getNickname()
+                )
         );
+    }
 
+    public record OwnerDto(
+            Long userId,
+            String nickname
+    ) {
     }
 }

04605d937701d4b8f6fd76de47c1013e12c1f71c
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-09-24 21:40:53 +0900

    refactor: 채팅 전송 POST 메서드로 구현 및 엔드포인트 수정 #191

--- src/main/java/com/example/daobe/chat/presentation/ChatController.java
+++ src/main/java/com/example/daobe/chat/presentation/ChatController.java
@@ -2,7 +2,6 @@ package com.example.daobe.chat.presentation;
 
 import com.example.daobe.chat.application.ChatService;
 import com.example.daobe.chat.application.dto.ChatMessageDto;
-import com.example.daobe.chat.application.dto.ChatMessageInfoDto;
 import com.example.daobe.chat.application.dto.ChatMessageResponseDto;
 import com.example.daobe.chat.application.dto.ChatRoomTokenDto;
 import com.example.daobe.common.response.ApiResponse;
@@ -11,67 +10,43 @@ import lombok.extern.slf4j.Slf4j;
 import org.bson.types.ObjectId;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
+@RequestMapping("/api/v1/chat-rooms")
 @RequiredArgsConstructor
 public class ChatController {
 
-    private static final String SUBSCRIBE_URL = "/sub/chat-rooms/%s/messages";
-
     private final ChatService chatService;
-    private final SimpMessagingTemplate messagingTemplate;
 
-    @MessageMapping("/chat-rooms/{roomToken}/enter")
-    public void enterChatRoom(
-            @DestinationVariable("roomToken") String token,
-            @Payload ChatMessageInfoDto message,
-            SimpMessageHeaderAccessor headerAccessor
+    @PostMapping("/chat/greet")
+    public void enterLeaveMessage(
+            @AuthenticationPrincipal Long userId,
+            @RequestBody ChatMessageDto messageDto
     ) {
-        ChatMessageInfoDto.EnterAndLeaveMessage newMessage = chatService.createEnterMessageAndSetSessionAttribute(
-                message,
-                token,
-                headerAccessor
-        );
-        messagingTemplate.convertAndSend(SUBSCRIBE_URL.formatted(token), newMessage);
+        chatService.sendEnterLeaveMessage(userId, messageDto);
     }
 
-    @MessageMapping("/chat-rooms/{roomToken}/messages")
+    @PostMapping("/chat")
     public void sendMessage(
-            @DestinationVariable("roomToken") String token,
-            @Payload ChatMessageDto message
+            @AuthenticationPrincipal Long userId,
+            @RequestBody ChatMessageDto messageDto
     ) {
-        messagingTemplate.convertAndSend(
-                SUBSCRIBE_URL.formatted(token),
-                chatService.createMessage(message, token)
-        );
+        chatService.sendMessage(userId, messageDto);
     }
 
-    @MessageMapping("/chat-rooms/{roomToken}/exit")
-    public void leaveChatRoom(
-            @DestinationVariable("roomToken") String token,
-            SimpMessageHeaderAccessor simpMessageHeaderAccessor
-    ) {
-        messagingTemplate.convertAndSend(
-                SUBSCRIBE_URL.formatted(token),
-                chatService.leaveChatRoom(simpMessageHeaderAccessor)
-        );
-    }
-
-    @GetMapping("/api/v1/chat-rooms/{objetId}/room-token")
+    @GetMapping("/{objetId}/room-token")
     public ResponseEntity<ApiResponse<ChatRoomTokenDto>> getChatRoomToken(
             @PathVariable(name = "objetId") Long objetId
     ) {
-        // TODO: 오브제에 소속된 사람만 조회 가능하도록 수정
         ChatRoomTokenDto roomToken = chatService.getRoomTokenByObjetId(objetId);
         ApiResponse<ChatRoomTokenDto> response = new ApiResponse<>(
                 "CHAT_ROOM_TOKEN_LOADED_SUCCESS",
@@ -80,7 +55,7 @@ public class ChatController {
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
-    @GetMapping("/api/v1/chat-rooms/{roomToken}/messages")
+    @GetMapping("/{roomToken}/messages")
     public ResponseEntity<ApiResponse<ChatMessageResponseDto>> getChatMessages(
             @PathVariable(value = "roomToken") String token,
             @RequestParam(value = "cursorId", required = false) ObjectId cursorId,
@@ -93,7 +68,7 @@ public class ChatController {
                 ));
     }
 
-    @GetMapping("/api/v1/chat-rooms/{roomToken}/messages/recent")
+    @GetMapping("/{roomToken}/messages/recent")
     public ResponseEntity<ApiResponse<ChatMessageResponseDto>> getRecentChatMessages(
             @PathVariable(value = "roomToken") String token
     ) {

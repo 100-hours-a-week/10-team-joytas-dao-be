4a5321c53279e6238e42dc6b3cf2e3295c00a086
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-09-24 21:38:37 +0900

    refactor: 채팅 관련 DTO 구조 변경 및 직렬화/역직렬화를 위한 설정 추가 #191

--- src/main/java/com/example/daobe/chat/application/dto/ChatMessageInfoDto.java
+++ src/main/java/com/example/daobe/chat/application/dto/ChatMessageInfoDto.java
@@ -2,18 +2,20 @@ package com.example.daobe.chat.application.dto;
 
 import com.example.daobe.chat.domain.ChatMessage;
 import com.example.daobe.chat.domain.ChatUser;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.time.LocalDateTime;
 
 public record ChatMessageInfoDto(
         String id,
         String type,
-        String roomToken,
-        Long senderId,
-        String senderName,
-        String senderProfileUrl,
+        @JsonProperty(value = "room_token") String roomToken,
+        @JsonProperty(value = "sender_id") Long senderId,
+        @JsonProperty(value = "sender_name") String senderName,
+        @JsonProperty(value = "sender_profile_url") String senderProfileUrl,
         String message,
-        LocalDateTime createdAt
+        @JsonProperty(value = "created_at") String createdAt
 ) {
+
     public static ChatMessageInfoDto of(ChatMessage message, ChatUser sender) {
         return new ChatMessageInfoDto(
                 message.getId().toHexString(),
@@ -23,16 +25,17 @@ public record ChatMessageInfoDto(
                 sender.getNickname(),
                 sender.getProfileUrl(),
                 message.getMessage(),
-                message.getCreatedAt()
+                message.getCreatedAt().toString()
         );
     }
 
     public record EnterAndLeaveMessage(
             String type,
-            String roomToken,
+            @JsonProperty(value = "room_token") String roomToken,
             String message,
-            LocalDateTime createdAt
+            @JsonProperty(value = "created_at") String createdAt
     ) {
+
         public static EnterAndLeaveMessage of(
                 String type,
                 String roomToken,
@@ -43,7 +46,7 @@ public record ChatMessageInfoDto(
                     type,
                     roomToken,
                     message,
-                    createdAt
+                    createdAt.toString()
             );
         }
     }

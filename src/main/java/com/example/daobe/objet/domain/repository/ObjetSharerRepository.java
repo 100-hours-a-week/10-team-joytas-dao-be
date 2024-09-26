6025c34c1d4122fcef1d7094ac0beb83fcf48869
jjikky <kok5417@skuniv.ac.kr>
2024-09-26 19:26:35 +0900

    chore: 불필요한 코드 삭제 #238

--- src/main/java/com/example/daobe/objet/domain/repository/ObjetSharerRepository.java
+++ src/main/java/com/example/daobe/objet/domain/repository/ObjetSharerRepository.java
@@ -2,24 +2,12 @@ package com.example.daobe.objet.domain.repository;
 
 import com.example.daobe.objet.domain.Objet;
 import com.example.daobe.objet.domain.ObjetSharer;
-import com.example.daobe.objet.domain.ObjetStatus;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface ObjetSharerRepository extends JpaRepository<ObjetSharer, Long> {
-
-    @Query("""
-            SELECT o FROM ObjetSharer os
-            JOIN os.objet o
-            WHERE os.user.id = :userId
-            AND o.status = :status
-            ORDER BY o.createdAt DESC
-            LIMIT 4
-            """)
-    List<Objet> findMyRecentObjetByUserId(@Param("userId") Long userId,
-                                          @Param("status") ObjetStatus status);
 
     List<ObjetSharer> findTop4ByUserIdOrderByIdDesc(Long userId);
 

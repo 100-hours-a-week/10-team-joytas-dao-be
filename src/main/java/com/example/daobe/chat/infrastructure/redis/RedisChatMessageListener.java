c33997b945f8d9ea0d63f954442e601824c9bf68
JiHongKim98 <kinjihong9598@gmail.com>
2024-09-30 16:24:42 +0900

    refactor: 레디스 이벤트 리스너 클래스 리팩토링 #251

--- src/main/java/com/example/daobe/chat/infrastructure/redis/RedisChatMessageListener.java
+++ src/main/java/com/example/daobe/chat/infrastructure/redis/RedisChatMessageListener.java
@@ -1,33 +1,33 @@
 package com.example.daobe.chat.infrastructure.redis;
 
-import static com.example.daobe.chat.exception.ChatExceptionType.CHAT_TRANSFER_FAILED_EXCEPTION;
-
-import com.example.daobe.chat.application.ChatMessageService;
+import com.example.daobe.chat.application.ChatMessagePublisher;
 import com.example.daobe.chat.application.dto.ChatMessageInfoDto;
-import com.example.daobe.chat.exception.ChatException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class RedisMessageListener implements MessageListener {
+public class RedisChatMessageListener implements MessageListener {
 
-    private final ChatMessageService chatMessageService;
-    private final ObjectMapper objectMapper;
-    private final RedisTemplate<String, Object> redisTemplate;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    private final ChatMessagePublisher chatMessagePublisher;
 
     @Override
     public void onMessage(Message message, byte[] pattern) {
-        String publishedMessage = redisTemplate.getStringSerializer().deserialize(message.getBody());
+        ChatMessageInfoDto payload = deserializeFromBytes(message.getBody());
+        chatMessagePublisher.execute(payload);
+    }
+
+    private ChatMessageInfoDto deserializeFromBytes(byte[] bytes) {
         try {
-            ChatMessageInfoDto chatMessage = objectMapper.readValue(publishedMessage, ChatMessageInfoDto.class);
-            chatMessageService.publishChatMessage(chatMessage);
-        } catch (Exception e) {
-            throw new ChatException(CHAT_TRANSFER_FAILED_EXCEPTION);
+            return objectMapper.readValue(bytes, ChatMessageInfoDto.class);
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
         }
     }
 }

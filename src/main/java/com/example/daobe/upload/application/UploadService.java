c0bb6ada7ad100c34e7ab9d91ac931a019412751
JiHongKim98 <kinjihong9598@gmail.com>
2024-09-30 16:50:50 +0900

    refactor: deprecated 된 메서드 삭제 #253

--- src/main/java/com/example/daobe/upload/application/UploadService.java
+++ src/main/java/com/example/daobe/upload/application/UploadService.java
@@ -1,6 +1,5 @@
 package com.example.daobe.upload.application;
 
-import com.example.daobe.upload.application.dto.UploadImageResponse;
 import com.example.daobe.upload.application.dto.UploadImageResponseDto;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +11,6 @@ import org.springframework.web.multipart.MultipartFile;
 public class UploadService {
 
     private final ImageClient imageClient;
-
-    @Deprecated(since = "2024-08-29")
-    public UploadImageResponse uploadImage(MultipartFile file) {
-        String objectKey = UUID.randomUUID().toString();
-        String imageUrl = imageClient.upload(objectKey, file);
-        return UploadImageResponse.of(imageUrl);
-    }
 
     public UploadImageResponseDto upload(MultipartFile file) {
         String objectKey = UUID.randomUUID().toString();

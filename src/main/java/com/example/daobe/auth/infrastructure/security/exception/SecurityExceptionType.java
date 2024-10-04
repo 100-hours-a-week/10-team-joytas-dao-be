ead63e5068ca30a8245a9e0ba4c02e01db3cf74c
JiHongKim98 <kinjihong9598@gmail.com>
2024-10-04 22:09:21 +0900

    refactor: 사용되지 않는 예외 타입 삭제 #279

--- src/main/java/com/example/daobe/auth/infrastructure/security/exception/SecurityExceptionType.java
+++ src/main/java/com/example/daobe/auth/infrastructure/security/exception/SecurityExceptionType.java
@@ -4,7 +4,6 @@ public enum SecurityExceptionType {
     UNAUTHORIZED("인증 정보가 제공되지 않았습니다."),
     INVALID_TOKEN("유효하지 않는 토큰입니다."),
     EXPIRED_TOKEN("이미 만료된 토큰입니다."),
-    INVALID_USER_INFO("유효하지 않는 사용자 이름 혹은 비밀번호 입니다."),
     ;
 
     private final String message;

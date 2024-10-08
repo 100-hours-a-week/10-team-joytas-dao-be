23443d2de42829187860ce8b6d1071e34f417edf
JiHongKim98 <kinjihong9598@gmail.com>
2024-10-08 22:32:38 +0900

    feat: 레디스 락을 사용하는 스케쥴러 설정 추가 #274

---
+++ src/main/java/com/example/daobe/common/config/ScheduleConfig.java
@@ -0,0 +1,20 @@
+package com.example.daobe.common.config;
+
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.redis.spring.RedisLockProvider;
+import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@Configuration
+@EnableScheduling
+@EnableSchedulerLock(defaultLockAtMostFor = "PT10S")
+public class ScheduleConfig {
+
+    @Bean
+    public LockProvider lockProvider(RedisConnectionFactory redisConnectionFactory) {
+        return new RedisLockProvider(redisConnectionFactory);
+    }
+}

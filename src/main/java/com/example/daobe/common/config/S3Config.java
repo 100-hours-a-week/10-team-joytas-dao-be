aaaabf7fe8cb6790a60591091ce2e3e9ef7d5bd6
JiHongKim98 <kinjihong9598@gmail.com>
2024-10-21 21:36:13 +0900

    feat: s3 presigned 설정 추가 #297

--- src/main/java/com/example/daobe/common/config/S3Config.java
+++ src/main/java/com/example/daobe/common/config/S3Config.java
@@ -8,6 +8,7 @@ import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
 @RequiredArgsConstructor
@@ -18,6 +19,14 @@ public class S3Config {
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
+                .credentialsProvider(awsCredentialsProvider())
+                .region(Region.of(properties.region()))
+                .build();
+    }
+
+    @Bean
+    public S3Presigner s3Presigner() {
+        return S3Presigner.builder()
                 .credentialsProvider(awsCredentialsProvider())
                 .region(Region.of(properties.region()))
                 .build();

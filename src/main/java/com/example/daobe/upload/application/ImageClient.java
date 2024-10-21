d8ecdab913ffa667cb1f7086021ca230e07e4900
JiHongKim98 <kinjihong9598@gmail.com>
2024-10-21 21:42:13 +0900

    feat: 이미지 업로드를 위한 URL 발급 로직 추가 #297

--- src/main/java/com/example/daobe/upload/application/ImageClient.java
+++ src/main/java/com/example/daobe/upload/application/ImageClient.java
@@ -5,4 +5,8 @@ import org.springframework.web.multipart.MultipartFile;
 public interface ImageClient {
 
     String upload(String objectKey, MultipartFile file);
+
+    String getUploadUrl(String objectKey);
+
+    String getImageUrl(String objectKey);
 }
